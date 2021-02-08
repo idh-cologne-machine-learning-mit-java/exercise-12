1e07886b86caea41eb7250d37294d6f383f1e0de
eliasuk <efaqirza@smail.uni-koeln.de>
2021-02-09 00:11:21 +0100

    Done

--- src/main/java/de/ukoeln/idh/teaching/jml/ex12/Exercise12Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex12/Exercise12Main.java
@@ -1,5 +1,4 @@
 package de.ukoeln.idh.teaching.jml.ex12;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,7 +9,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
@@ -25,6 +23,7 @@ import org.datavec.api.transform.transform.string.StringListToCountsNDArrayTrans
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
@@ -33,66 +32,58 @@ import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
-public class Exercise12Main {
 
+public class Exercise12Main {
 	public static void main(String[] args) throws Exception {
 		File trainFile = new File("src/main/resources/amazon/train_small.ft.txt.bz2");
 		File testFile = new File("src/main/resources/amazon/test_small.ft.txt.bz2");
-
 		MyRecordReader trainReader = new MyRecordReader();
 		trainReader.initialize(new FileSplit(trainFile));
 		MyRecordReader testReader = new MyRecordReader();
 		testReader.initialize(new FileSplit(testFile));
-
 		Schema dataSchema = new Schema.Builder().addColumnString("label").addColumnString("text").build();
-
 		List<String> vocabulary = readVocabulary(testFile, 15);
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("\\d", " ");
 		map.put("\\p{Punct}", " ");
-
 		TransformProcess tp = new TransformProcess.Builder(dataSchema)
 				.transform(new ChangeCaseStringTransform("text", ChangeCaseStringTransform.CaseType.LOWER))
 				.transform(new ReplaceStringTransform("text", map))
 				.transform(new StringListToCountsNDArrayTransform("text", vocabulary, " ", false, true))
 				.transform(new StringToCategoricalTransform("label", "__label__2", "__label__1"))
 				.transform(new CategoricalToIntegerTransform("label")).build();
-
 		RecordReader procTrainReader = new TransformProcessRecordReader(trainReader, tp);
 		RecordReader procTestReader = new TransformProcessRecordReader(testReader, tp);
 		DataSetIterator trainDataSetIterator = new RecordReaderDataSetIterator(procTrainReader, 5, 0, 2);
 		DataSetIterator testDataSetIterator = new RecordReaderDataSetIterator(procTestReader, 5, 0, 2);
-
 		int rngSeed = 123;
 		int numColumns = vocabulary.size();
 
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(rngSeed).list().layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
 						.nIn(numColumns).nOut(2).activation(Activation.SOFTMAX).build())
+				.seed(rngSeed).list()
+				.layer(new DenseLayer.Builder().activation(Activation.SIGMOID).nIn(numColumns).nOut(200).build())
+				.layer(new DenseLayer.Builder().activation(Activation.TANH).nIn(200).nOut(100).build())
+				.layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+						.nIn(100).nOut(2).activation(Activation.SOFTMAX).build())
 				.validateOutputLayerConfig(true).build();
 		MultiLayerNetwork network = new MultiLayerNetwork(conf);
 		network.init();
-
 		int eachIterations = 100;
 		network.addListeners(new ScoreIterationListener(eachIterations));
-
 		network.fit(trainDataSetIterator);
-
 		Evaluation eval = network.evaluate(testDataSetIterator);
-
 		System.out.println("Loss " + network.score());
 		System.out.println("Accuracy: " + eval.accuracy());
 		System.out.println("Precision: " + eval.precision());
 		System.out.println("Recall: " + eval.recall());
-
 	}
-
 	public static List<String> readVocabulary(File file, int limit) throws FileNotFoundException, IOException {
 		Map<String, Integer> vocab = new HashMap<String, Integer>();
 		System.err.println("Reading vocabulary ... ");
 		try (InputStreamReader reader = new InputStreamReader(
 				new BZip2CompressorInputStream(new FileInputStream(file)))) {
-
 			StreamTokenizer tokenizer = new StreamTokenizer(reader);
 			tokenizer.slashSlashComments(false);
 			tokenizer.slashStarComments(false);
@@ -113,9 +104,7 @@ public class Exercise12Main {
 		for (String w : vocab.keySet()) {
 			if (vocab.get(w) >= limit)
 				finalVocab.add(w);
-
 		}
-
 		System.err.println("Done. Vocabulary contains " + finalVocab.size() + " words.");
 		return finalVocab;
 	}
--- src/main/java/de/ukoeln/idh/teaching/jml/ex12/Titanic.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex12/Titanic.java
@@ -1,5 +1,4 @@
 package de.ukoeln.idh.teaching.jml.ex12;
-
 import java.io.File;
 
 import org.datavec.api.records.reader.RecordReader;
@@ -24,49 +23,38 @@ import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-
 public class Titanic {
-
 	public static void main(String[] args) throws Exception {
 		// the data file
 		File datafile = new File("src/main/resources/titanic.csv");
-
 		// a record reader
 		RecordReader reader = new CSVRecordReader(1, ',', '"');
 		reader.initialize(new FileSplit(datafile));
-
 		// the schema
 		Schema dataSchema = new Schema.Builder().addColumnsInteger("PassengerId", "Survived", "Pclass")
 				.addColumnsString("Name").addColumnCategorical("Sex", "male", "female", "").addColumnInteger("Age")
 				.addColumnsInteger("SipSp", "Parch").addColumnsString("Ticket").addColumnDouble("Fare")
 				.addColumnsString("Cabin").addColumnCategorical("Embarked", "C", "S", "Q", "").build();
-
 		// transformation: remove string and unhelpful columns, transform
 		// categories into int values
 		TransformProcess tp = new TransformProcess.Builder(dataSchema)
 				.transform(new RemoveColumnsTransform("PassengerId", "Name", "Ticket", "Cabin"))
 				.transform(new CategoricalToIntegerTransform("Sex"))
 				.transform(new CategoricalToIntegerTransform("Embarked")).build();
-
 		// Create a reader to read from the transform process
 		RecordReader procReader = new TransformProcessRecordReader(reader, tp);
-
 		// we are only interested in the first data set
 		DataSetIterator trainDataSetIterator = new RecordReaderDataSetIterator(procReader, 892, 0, 2);
 		DataSet allData = trainDataSetIterator.next();
 		allData.shuffle();
-
 		// split into training and test
 		SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);
 		DataSet trainingData = testAndTrain.getTrain();
 		DataSet testData = testAndTrain.getTest();
-
 		// number of input features
 		final int numInputs = tp.getFinalSchema().numColumns() - 1;
-
 		// number of output categories
 		int outputNum = allData.numOutcomes();
-
 		// seed value for the RNG, fixed for reproducibility
 		long seed = 6;
 
@@ -76,6 +64,12 @@ public class Titanic {
 				.layer(new DenseLayer.Builder().nIn(100).nOut(100).build())
 				.layer(new DenseLayer.Builder().nIn(100).nOut(100).build())
 				.layer(new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX)
+		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder().seed(seed).activation(Activation.RELU).list()
+				.layer(new DenseLayer.Builder().nIn(numInputs).nOut(100).dropOut(0.9).l2(0.01).build())
+				.layer(new DenseLayer.Builder().nIn(100).nOut(100).dropOut(0.9).l2(0.01).build())
+				.layer(new DenseLayer.Builder().nIn(100).nOut(20).dropOut(0.9).l2(0.01).build())
+				.layer(new DenseLayer.Builder().nIn(20).nOut(100).dropOut(0.9).l2(0.01).build())
+				.layer(new OutputLayer.Builder(LossFunctions.LossFunction.MEAN_SQUARED_LOGARITHMIC_ERROR).activation(Activation.SOFTMAX)
 						.nIn(100).nOut(outputNum).build())
 				.build();
 
@@ -83,17 +77,14 @@ public class Titanic {
 		MultiLayerNetwork model = new MultiLayerNetwork(conf);
 		model.init();
 		model.setListeners(new ScoreIterationListener(10));
-
 		// start training of 1000 epochs
 		for (int i = 0; i < 1000; i++) {
 			model.fit(trainingData);
 		}
-
 		// evaluate the model on the test set
 		Evaluation eval = new Evaluation(2);
 		INDArray output = model.output(testData.getFeatures());
 		eval.eval(testData.getLabels(), output);
 		System.out.println(eval.stats());
 	}
-
 }
